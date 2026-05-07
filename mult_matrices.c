dbb1dc3f6e1cc856db889d0061d7a7213f8b06f1
M1gv3lMiran <mirandamiguek715@gmail.com>
2026-05-07 12:00:17 +0000

    He hecho varios cambios     - En primer lugar se han cambiado lo de las matrices de A[MAX_SIZE][MAX_SIZE] a A[tam][tam] para que se pueda usar con cualquier tamaño de matriz.     - cambiar de A[tam][tam] a A[tam*tam] por lo que ha dicho joaquin     - Se ha modificado la funcion de multiplicacion de matrices

--- mult_matrices.c
+++ mult_matrices.c
@@ -3,28 +3,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_TAM 512
+#define MAX_TAM 1024
 
-int A[MAX_TAM][MAX_TAM];
-int B[MAX_TAM][MAX_TAM];
-int C[MAX_TAM][MAX_TAM];
-
-void rellenar_matriz(int mat[MAX_TAM][MAX_TAM], int tam)
+//Funcion encargada de rellenar la matriz
+//Argumentos:
+// -mat: matriz a rellenar en formato 1D
+// -tam: tamaño de la matriz (n x n)
+//Funcion de tipo void por lo cual no retorna nada
+void rellenar_matriz(int *mat, int tam)
 {   
     for (int i = 0; i < tam; i++)
         for (int j = 0; j < tam; j++)
-            mat[i][j] = (rand() % 10) + 1;
+            mat[i * tam + j] = (rand() % 10) + 1;
 }
 
-void multiplicar_filas(int local_C[MAX_TAM][MAX_TAM], int local_A[MAX_TAM][MAX_TAM],
-                   int tam, int num_rows)
+//Funcion encargada de multiplicar filas de las matrices
+//Argumentos:
+// -local_C: matriz resultado local en formato 1D
+// -local_A: matriz A local en formato 1D
+// -tam: tamaño de la matriz (n x n)
+// -num_rows: número de filas que procesa cada nodo
+//Funcion de tipo void por lo cual no retorna nada
+void multiplicar_filas(int *local_C, int *local_A,int *B, int tam, int num_rows)
 {
     for (int i = 0; i < num_rows; i++)
         for (int j = 0; j < tam; j++)
         {
-            local_C[i][j] = 0;
+            local_C[i * tam + j] = 0;
             for (int k = 0; k < tam; k++)
-                local_C[i][j] += local_A[i][k] * B[k][j];
+                local_C[i * tam + j] += local_A[i * tam + k] * B[k * tam + j];
         }
 }
 /*
@@ -40,6 +47,11 @@ void imprimir_matriz(int mat[MAX_TAM][MAX_TAM], int tam)
 */
 
 //Control de errores para el tamaño que pasamos por línea de argumentos en función número de nodos
+//Argumentos:
+// -argc: número de argumentos pasados por línea de comandos
+// -argv: vector de argumentos pasados por línea de comandos
+// -world_size: número de nodos en la ejecución MPI
+// -tam: puntero a la variable donde se almacenará el tamaño de la matriz
 //La funcion retorna 1 si es correcto el control de errores , 0 si es incorrecto
 int comprobar_argumentos(int argc, char **argv, int world_size, int *tam)
 {
@@ -69,17 +81,19 @@ int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
 
-    int world_rank, world_size;
+    int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     int tam;
     int valid = 0;
 
-    if (world_rank == 0)
-    {
-        valid = comprobar_argumentos(argc, argv, world_size, &tam);
-    }
+    valid = comprobar_argumentos(argc, argv, world_size, &tam);
+
+    int A[tam*tam];
+    int B[tam*tam];
+    int C[tam*tam];
 
     MPI_Bcast(&valid, 1, MPI_INT, 0, MPI_COMM_WORLD);
     if (!valid)
@@ -92,8 +106,8 @@ int main(int argc, char **argv)
 
     int rows_per_process = tam / world_size;
 
-    int local_A[MAX_TAM][MAX_TAM];
-    int local_C[MAX_TAM][MAX_TAM];
+    int local_A[tam * tam];
+    int local_C[tam * tam];
 
     //nodo 0 inicializa las matriz
     if (world_rank == 0)
@@ -101,22 +115,24 @@ int main(int argc, char **argv)
         srand(time(NULL));
         rellenar_matriz(A, tam);
         rellenar_matriz(B, tam);
+
+        printf("Cantidad de Kbytes ocupados por cada una de las matrices: %zu KB\n", tam * tam * sizeof(int) / 1024);
     }
 
-    MPI_Bcast(B, MAX_TAM * MAX_TAM, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(B, tam * tam, MPI_INT, 0, MPI_COMM_WORLD);
 
-    MPI_Scatter(A, rows_per_process * MAX_TAM, MPI_INT,
-                local_A, rows_per_process * MAX_TAM, MPI_INT,
+    MPI_Scatter(A, rows_per_process * tam, MPI_INT,
+                local_A, rows_per_process * tam, MPI_INT,
                 0, MPI_COMM_WORLD);
 
     double t_inicio = MPI_Wtime();
 
-    multiplicar_filas(local_C, local_A, tam, rows_per_process);
+    multiplicar_filas(local_C, local_A, B, tam, rows_per_process);
 
     double t_fin = MPI_Wtime();
 
-    MPI_Gather(local_C, rows_per_process * MAX_TAM, MPI_INT,
-               C, rows_per_process * MAX_TAM, MPI_INT,
+    MPI_Gather(local_C, rows_per_process * tam, MPI_INT,
+               C, rows_per_process * tam, MPI_INT,
                0, MPI_COMM_WORLD);
 
     if (world_rank == 0)
